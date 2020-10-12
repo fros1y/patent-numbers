bd28d2f2800f7563ace44ded28cc70d3ec902186
Martin Galese <martin@galese.net>
2020-10-12 09:22:28 -0400

    Create the project

---
+++ src/PatentNumbers.hs
@@ -0,0 +1,15 @@
+{- |
+Copyright: (c) 2020 Martin Galese
+SPDX-License-Identifier: Apache-2.0
+Maintainer: Martin Galese <martin@patentmark.ai>
+
+Parse and store patent publication reference numbers
+-}
+
+module PatentNumbers
+       ( someFunc
+       ) where
+
+
+someFunc :: IO ()
+someFunc = putStrLn ("someFunc" :: String)
